64d8b94577624e540f34a59c7ea384cefca8ba0d
carloscarvallo <erotomania669@gmail.com>
2016-06-17 01:36:26 -0400

    using const

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
-var request = require('request');
-var express = require('express');
-var app = express();
-var config = require('./config');
+const request = require('request'),
+	  express = require('express'),
+	  app = express(),
+	  config = require('./config');
 
 require('dotenv').config();
 
-var port = process.env.PORT || 8080;
-var url = config.serviceUrl;
-var apigeeToken = process.env.APIGEE_TOKEN;
+const port = process.env.PORT || 8080,
+	  url = config.serviceUrl,
+	  apigeeToken = process.env.APIGEE_TOKEN;
 
-var options = {
+const options = {
 	url: url,
 	auth: {
 		'bearer': apigeeToken
 	}
 };
 
-var middleware = (req, res, next) => {
+const middleware = ( req, res, next ) => {
 	console.log('...Something happen');
 	next();
 };
 
-var greet = (req, res) => {
+const greet = ( req, res ) => {
 	res.json({ mensaje: "bienvenido a mi API" });
 };
 
-var getService = (req, res) => {
-	request(options, (err, response) => {
-		res.json(JSON.parse(response.body));
+const getService = ( req, res ) => {
+	request(options, ( err, response ) => {
+		res.json(JSON.parse( response.body ));
 	});
 };
 
-var router = express.Router();
+const router = express.Router();
 
-router.use(middleware);
+router.use( middleware );
 
 router.get('/', greet);
 
 router.route('/users')
-	.get(getService);
+	.get( getService );
 	
 app.use('/api', router);
 
