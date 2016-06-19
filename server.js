fad99e85e78e669942683e0dd00afa67da8697da
carloscarvallo <erotomania669@gmail.com>
2016-06-19 12:12:01 -0400

    define variables

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 const request = require('request'),
 	  express = require('express'),
 	  app = express(),
-	  morgan = require('morgan');
-	  config = require('./config');
+	  bodyParser = require('body-parser'),
+	  morgan = require('morgan'),
+	  mongoose = require('mongoose'),
+	  jwt = require('jsonwebtoken'),
+	  User = require('./app/models/user'),
+	  config = require('./config'),
+	  
+	  port = process.env.PORT || 8080;
+	  
+mongoose.connect(config.database);
+
+app.set('superSecret', config.secret);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+// connection test
+const db = mongoose.connection;
+db.on('error', console.error.bind( console, 'connection error: ' ));
+db.on('open', function() {
+    console.log('Conectado a la base de datos');
+});
 
 /*
 require('dotenv').config();
